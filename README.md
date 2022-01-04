1d20c598308ed02f71b84414c1fcb70c6eeeb768
Jocker271 <git@johanneswehden.de>
2022-01-04 12:03:53 +0100

    [ADD] template.py and readme

---
+++ README.md
@@ -0,0 +1,33 @@
+# Advent of Code (Python)
+This are my solutions for the yearly [Advent of Code](https://adventofcode.com/) challenges.
+
+[![License](https://img.shields.io/badge/License-MIT-green)](LICENSE)
+![Python](https://img.shields.io/badge/Python-v3.9-informational)
+
+## About this repository
+Of course, many solutions can be written shorter and probably more efficiently, but on the one hand I want to write code that is easy to understand and on the other hand spare [PyLint's](https://pylint.org/) nerves. 😉
+
+As I only came across this project in 2020, some tasks are still unresolved. In addition, every year I have the problem of not finding time for the tasks after a few days, so that there is a backlog that can only be made up later. However, I am willing to catch up on all tasks as soon as my time and motivation allow.
+
+### Challenge Progress
+<!-- 0%lightgrey 2%+orange 25%+yellow 50%+yellowgreen 75%+green 100%brightgreen -->
+[![Progress 2015](https://img.shields.io/badge/2015-16%25-orange)](2015) 
+[![Progress 2016](https://img.shields.io/badge/2016-0%25-lightgrey)](2016) 
+[![Progress 2017](https://img.shields.io/badge/2017-0%25-lightgrey)](2017) 
+[![Progress 2018](https://img.shields.io/badge/2018-12%25-orange)](2018) 
+[![Progress 2019](https://img.shields.io/badge/2019-32%25-yellow)](2019) 
+[![Progress 2020](https://img.shields.io/badge/2020-40%25-yellow)](2020) 
+[![Progress 2021](https://img.shields.io/badge/2021-16%25-orange)](2021)
+
+## Template
+To avoid creating the new files every day, I wrote a script ([template.py](template.py)) that creates all the necessary folders and files of the entered year.
+
+## Contact
+Johannes Wehden - [Jocker271](https://github.com/Jocker271) - aoc@johanneswehden.de
+
+Project Link: [https://github.com/Jocker271/advent-of-code](https://github.com/Jocker271/advent-of-code)
+
+## Acknowledgments
+Special thanks to Eric Wastl for the annual Advend of Code puzzle fun.
+
+Suggestions for improvement are welcome.
---
+++ template.py
@@ -0,0 +1,92 @@
+'''
+Created on 2021-12-01
+by Johannes Wehden - (Jocker271)
+--------------------------------
+This script creates all folders and template files for the entered year,
+if they do not already exist. The Python template is at the end of this file.
+'''
+
+import os
+from datetime import datetime
+
+def main():
+    '''Retrieves user input and executes this script.'''
+    while True:
+        print('For which year do you want to create the file structure?')
+        year = input("Year: ") or str(datetime.now().year)
+        if year.isdigit() and len(year) == 4:
+            create_file_structure(year)
+            break
+        print(f'{year} is not a possible value.')
+    print('✓ File structure successfully created')
+
+def create_file_structure(year):
+    '''Creates python and text files for each day in a seperate folder.'''
+    parent = os.getcwd()
+    folder_path = os.path.join(parent, year)
+    if not os.path.exists(folder_path):
+        os.mkdir(folder_path)
+    template = get_template().replace('{year}', year)
+    for day in range(1, 26):
+        day_folder = os.path.join(folder_path, f'day_{str(day)}')
+        if not os.path.exists(day_folder):
+            os.mkdir(day_folder)
+            py_file_path = os.path.join(day_folder, f'day_{str(day)}.py')
+            txt_file_path = os.path.join(day_folder, 'input.txt')
+            with open(py_file_path, encoding='utf-8', mode='w+') as py_file:
+                py_file.write(template.replace('{day}', str(day)))
+                #py_file.close()
+            open(txt_file_path, encoding='utf-8', mode='x').close()
+
+def get_template():
+    '''Returns template inside this file (look at the end) as String.'''
+    template = ''
+    with open(__file__, encoding='utf-8', mode='r') as this_file:
+        start_template = False
+        for line in this_file:
+            if start_template:
+                template += line[2:] if len(line) > 2 else '\n'
+            elif line == '# --- TEMPLATE STARTS HERE ---\n':
+                start_template = True
+    return template
+
+main()
+
+# --- TEMPLATE STARTS HERE ---
+# '''
+# Created on {year}-12-{day}
+# by Johannes Wehden - (Jocker271)
+# --------------------------------
+# Find this challenge at https://adventofcode.com/{year}/day/{day}
+# '''
+
+# import os
+
+# def get_challenge_input():
+#     '''Read input.txt and return content as List'''
+#     input_file = f'{os.path.dirname(__file__)}\\input.txt'
+#     with open(input_file, encoding='utf-8', mode='r') as file:
+#         lines = file.read().split('\n')
+#         # lines = list(map(int, lines)) #convert Strings to Integers
+#         return lines
+
+# def part_one(puzzle):
+#     '''Solving the first challenge'''
+#     result = 0
+#     for line in puzzle:
+#         if line:
+#             result += 1
+#     return str(result)
+
+# def part_two(puzzle):
+#     '''Solving the second challenge'''
+#     result = 0
+#     for idx, line in enumerate(puzzle):
+#         if line:
+#             result = idx
+#             break
+#     return str(result)
+
+# puzzle = get_challenge_input()
+# print(f'Part 1: {part_one(puzzle)}')
+# print(f'Part 2: {part_two(puzzle)}')
